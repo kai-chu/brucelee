329e7241a40e23c150f6c778818ae3f72409fdc2
Kai Chu <chuck.chu.liu@gmail.com>
2019-10-07 19:05:55 +0200

    update devserver configuration

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -2,17 +2,24 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   plugins: [
-    new HtmlWebpackPlugin({template: './public/index.html'})
+    new HtmlWebpackPlugin({title: "Development", template: './public/index.html'})
   ],
   module: {
     rules: [
       { test: /\.txt$/, use: 'raw-loader' }
     ]
-  }
+  },
+  // dev server configurations
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
 };
