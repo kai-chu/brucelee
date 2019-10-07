b22b273a3dd7a20c2d618ce59ee2820efb2de276
Kai Chu <chuck.chu.liu@gmail.com>
2019-10-07 19:25:40 +0200

    change phaser to npm package

--- ui/src/app.js
+++ ui/src/app.js
@@ -1,5 +1,5 @@
 import {Client} from '@heroiclabs/nakama-js';
-
+import Phaser from 'phaser';
 var client = new Client("defaultkey", "127.0.0.1", 7350);
 client.ssl = false;
 
@@ -16,3 +16,53 @@ async function init() {
 }
 
 init()
+
+function preload() {
+  this.load.setBaseURL('http://labs.phaser.io');
+
+  this.load.image('sky', 'assets/skies/space3.png');
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+  this.load.image('red', 'assets/particles/red.png');
+}
+
+function create() {
+  this.add.image(400, 300, 'sky');
+
+  var particles = this.add.particles('red');
+
+  var emitter = particles.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+  });
+
+  var logo = this.physics.add.image(400, 100, 'logo');
+
+  logo.setVelocity(100, 200);
+  logo.setBounce(1, 1);
+  logo.setCollideWorldBounds(true);
+
+  emitter.startFollow(logo);
+}
+
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 200 }
+      }
+  },
+  scene: {
+      preload: preload,
+      create: create
+  }
+};
+
+function loadGame() {
+  var game = new Phaser.Game(config);
+}
+
+loadGame();
